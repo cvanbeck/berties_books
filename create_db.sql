982013fd5ef4e18aef3c4e67bd138d8df9803be6
Charlie <charlievanbeck@gmail.com>
2025-11-19 11:45:39 +0000

    Add password and username registration, including hashing for password stored on database

--- create_db.sql
+++ create_db.sql
@@ -11,5 +11,15 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
 
+CREATE TABLE IF NOT EXISTS userData (
+    id              INT AUTO_INCREMENT,
+    username        VARCHAR(50),
+    firstName       VARCHAR(50),
+    lastName        VARCHAR(50),
+    email           VARCHAR(50),
+    hashedPassword  TINYTEXT, 
+    PRIMARY KEY(id)
+);
+
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop';
 GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,38 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const bcrypt = require('bcrypt');
+
+// Encryption config
+const saltRounds = 10;
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 })
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    const plainPassword = req.body.password;
+    let hash;
+    bcrypt.hash(plainPassword, saltRounds, (err, hashedPassword) => {
+        if (err) {
+            res.send(err)
+        }
+        else {
+            hash = hashedPassword
+            let sqlQuery = "INSERT INTO userData (username, firstName, lastName, email, hashedPassword) VALUES (?, ?, ?, ?, ?)"
+            let newRecord = [req.body.username, req.body.first, req.body.last, req.body.email, hash];
+            db.query(sqlQuery, newRecord, (err, result) => {
+                if (err) {
+                    res.send(err)
+                }
+                else {
+                res.send(`Hello ${req.body.first} ${req.body.last} you are now registered!
+                     We will send an email to you at ${req.body.email}. Your password is:
+                     ${req.body.password} and your hashed password is: ${hash}
+                    `)};                                                                              
+            });
+        }
+    });
 }); 
 
 // Export the router object so index.js can access it
